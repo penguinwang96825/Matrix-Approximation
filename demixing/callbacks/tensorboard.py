4ddf3250e03adf30f73d4cd7854bd0fef40fa87c
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-12-28 21:07:57 +0000

    add SummaryWriter

--- demixing/callbacks/tensorboard.py
+++ demixing/callbacks/tensorboard.py
@@ -1,11 +1,31 @@
+import torch
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard.summary import hparams
 from .callback import Callback
+
+
+class SummaryWriter(SummaryWriter):
+
+    def add_hparams(self, hparam_dict, metric_dict):
+        torch._C._log_api_usage_once("tensorboard.logging.add_hparams")
+        if type(hparam_dict) is not dict or type(metric_dict) is not dict:
+            raise TypeError('hparam_dict and metric_dict should be dictionary.')
+        exp, ssi, sei = hparams(hparam_dict, metric_dict)
+
+        logdir = self._get_file_writer().get_logdir()
+        
+        with SummaryWriter(log_dir=logdir) as w_hp:
+            w_hp.file_writer.add_summary(exp)
+            w_hp.file_writer.add_summary(ssi)
+            w_hp.file_writer.add_summary(sei)
+            for k, v in metric_dict.items():
+                w_hp.add_scalar(k, v)
 
 
 class TensorBoardLogger(Callback):
     
     def __init__(self, log_dir=".logs/"):
-        self.writer = SummaryWriter(log_dir=log_dir, flush_secs=30)
+        self.writer = SummaryWriter(log_dir=log_dir, flush_secs=1, filename_suffix='')
 
     def on_train_epoch_end(self, model):
         for metric in model.metrics["train"]:
