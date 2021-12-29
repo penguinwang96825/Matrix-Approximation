edf9ef991e3e55ee56b2a312cb0e60ab203e98eb
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-12-29 15:20:45 +0000

    add reduction (mean) to apply to loss

--- demixing/nnets/loss/pce.py
+++ demixing/nnets/loss/pce.py
@@ -10,6 +10,11 @@ class PermutationCrossEntropy(nn.Module):
         super(PermutationCrossEntropy, self).__init__()
         self.num_perm = num_perm
 
+    def get_combinations(self):
+        comb = permutations(range(self.num_perm), self.num_perm)
+        comb = list(comb)
+        return comb
+
     def forward(self, preds, targets):
         """
         Parameters
@@ -18,15 +23,37 @@ class PermutationCrossEntropy(nn.Module):
             Size of [batch_size, num_speaker_to_demix, num_classes]
         targets: torch.tensor
             Size of [batch_size, num_speaker_to_demix]
-        """
-        comb = permutations(range(self.num_perm), self.num_perm)
-        comb = list(comb)
 
+        Examples
+        --------
+        >>> BATCH_SIZE = 2
+        >>> NUM_CLASSES = 5
+        >>> NUM_SPEAKER_TO_DEMIX = 3
+        >>> y_true = torch.LongTensor([
+        ...     [0, 1, 2], 
+        ...     [0, 2, 4]
+        ... ])
+        >>> y_pred = torch.FloatTensor([
+        ...     [
+        ...         [0.1, 0.2, 0.3, 0.4, 0.5], 
+        ...         [0.4, 0.5, 0.1, 0.6, 0.1], 
+        ...         [0.6, 0.4, 0.3, 0.3, 0.5]
+        ...     ], 
+        ...     [
+        ...         [0.7, 0.6, 0.5, 0.4, 0.3], 
+        ...         [0.1, 0.3, 0.5, 0.7, 0.9], 
+        ...         [0.5, 0.7, 0.9, 0.1, 0.3]
+        ...     ]
+        ... ])
+        >>> pce = PermutationCrossEntropy(NUM_SPEAKER_TO_DEMIX)
+        >>> loss = pce(y_pred, y_true)
+        ... tensor([[4.5261], 
+        ...         [3.9176]])
+        """
         # Iterate through every sample in one batch
+        comb = self.get_combinations()
         loss_batch = []
         for gt, pb in zip(targets, preds):
-            comb = permutations(range(self.num_perm), self.num_perm)
-            comb = list(comb)
             losses = []
             for c in gt[..., comb]:
                 loss = torch.tensor([0.0])
@@ -35,5 +62,4 @@ class PermutationCrossEntropy(nn.Module):
                 losses.append(loss)
             loss_batch.append(min(losses))
         loss_batch = torch.stack(loss_batch)
-
-        return loss_batch
+        return torch.mean(loss_batch)
