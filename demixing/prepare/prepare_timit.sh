7e6bdb09c182aa269e7623cf36d95748db671f22
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-12-29 13:49:39 +0000

    remove files

--- demixing/prepare/prepare_timit.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p data
-mkdir -p data/corpus
-mkdir -p data/dataset
-mkdir -p data/corpus/timit
-mkdir -p data/dataset/timit
-
-pythonexec=C:/Users/Yang/anaconda3/envs/nlp/python.exe
-$pythonexec $PWD/demixing/prepare/prepare_timit.py
