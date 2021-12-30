f38301069434ba9cec7d1b0fe5d02ef042bd6fca
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-12-30 02:09:01 +0000

    update init

--- demixing/feats/__init__.py
+++ demixing/feats/__init__.py
@@ -3,3 +3,5 @@ from .transforms import MelSpectrogram
 from .transforms import MelScale
 from .transforms import Spectrogram
 from .transforms import AmplitudeToDB
+from .transforms import DCT
+from .transforms import Deltas
