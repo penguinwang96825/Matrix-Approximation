e48486c52c150f79fb7b3808b79e69728c5b7749
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-01-14 00:44:11 +0000

    remove loss and metrics from forward

--- train_multi_label.py
+++ train_multi_label.py
@@ -1,11 +1,13 @@
 import os
 import argparse
 import torch
+import json
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 from pathlib import Path
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from collections import OrderedDict
 from demixing import set_seed
 from demixing.datasets import SpeechDataset
@@ -69,9 +71,9 @@ class DecompositionNet(Module):
 
         self.step_scheduler_after = "batch"
 
-    def forward(self, mfcc, speaker_1=None, speaker_2=None):
+    def forward(self, batch):
         # mfcc: [batch, n_mfcc, time]
-        mfcc = mfcc.transpose(1, 2)
+        mfcc = batch['mfcc'].transpose(1, 2)
         # mixed_emb: [batch, emb_dim]
         mixed_emb = self.encoder(mfcc)
         # mixed_emb:[batch, emb_dim, 1]
@@ -89,11 +91,19 @@ class DecompositionNet(Module):
         outputs = self.classifier(pca)
         outputs = torch.sigmoid(outputs)
 
+        return outputs
+
+    def compute_objectives(self, output, batch):
         # targets: [batch, num_classes]
+        speaker_1, speaker_2 = batch['speaker_1'], batch['speaker_2']
         targets = F.one_hot(speaker_1, num_classes=630) + F.one_hot(speaker_2, num_classes=630)
-        loss = self.loss_fn(outputs, targets.float())
-        m = self.monitor_metrics(outputs, targets)
-        return outputs, loss, m
+        return self.loss_fn(output, targets.float())
+
+    def compute_metrics(self, output, batch):
+        # targets: [batch, num_classes]
+        speaker_1, speaker_2 = batch['speaker_1'], batch['speaker_2']
+        targets = F.one_hot(speaker_1, num_classes=630) + F.one_hot(speaker_2, num_classes=630)
+        return self.monitor_metrics(output, targets)
 
     def monitor_metrics(self, outputs, targets):
         if targets is None:
@@ -117,6 +127,16 @@ def hamming_score(y_true, y_pred):
     return np.mean(acc_list)
 
 
+def load_jsonl(jsonl_path):
+    speakers, waveforms = [], []
+    with open(jsonl_path, 'r') as f:
+        for line in f:
+            observation = json.loads(line)
+            speakers.append(observation['speakers'])
+            waveforms.append(observation['waveforms'])
+    return waveforms, speakers
+
+
 def main():
     args = argparse.ArgumentParser(
         description="Preparing TIMIT dataset for training."
@@ -126,15 +146,21 @@ def main():
         "--augmentation",
         default=0,
         type=int,
-        help="Number of mixture",
+        help="Number of augmentation",
     )
     args = args.parse_args()
 
-    NUM_TO_DEMIX = 2
-
+    # NUM_TO_DEMIX = 2
+    CHECKPOINTS_PATH = os.path.join(CHECKPOINTS_ROOT, f'svd-aug{args.augmentation}-bs128-lr3-adamw-noam.ckpt')
     TRAIN_JSONL_PATH = os.path.join(TIMIT_DATASET_ROOT, 'train-clean-2mix.jsonl')
-    ds = SpeechDataset(
-        TRAIN_JSONL_PATH, 
+    TEST_JSONL_PATH = os.path.join(TIMIT_DATASET_ROOT, 'test-clean-2mix.jsonl')
+
+    X_train, y_train = load_jsonl(TRAIN_JSONL_PATH)
+    X_test, y_test = load_jsonl(TEST_JSONL_PATH)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=914)
+
+    train_ds = SpeechDataset(
+        X_train, y_train, 
         sr=CONFIG['sample_rate'], 
         n_mfcc=CONFIG['n_mfcc'], 
         snr=CONFIG['snr'], 
@@ -142,11 +168,29 @@ def main():
         augmentation=args.augmentation, 
         mfcc_transform=True
     )
-    train_ds, valid_ds = torch.utils.data.random_split(ds, [round(len(ds)*.8), round(len(ds)*.2)])
+    valid_ds = SpeechDataset(
+        X_valid, y_valid, 
+        sr=CONFIG['sample_rate'], 
+        n_mfcc=CONFIG['n_mfcc'], 
+        snr=CONFIG['snr'], 
+        slice_dur=CONFIG['slice_dur'], 
+        augmentation=0, 
+        mfcc_transform=True
+    )
+    test_ds = SpeechDataset(
+        X_test, y_test, 
+        sr=CONFIG['sample_rate'], 
+        n_mfcc=CONFIG['n_mfcc'], 
+        snr=CONFIG['snr'], 
+        slice_dur=CONFIG['slice_dur'], 
+        augmentation=5, 
+        mfcc_transform=True
+    )
 
     print(
         f'TRAIN: {len(train_ds)}\t'
-        f'VALID: {len(valid_ds)}'
+        f'VALID: {len(valid_ds)}\t'
+        f'TEST: {len(test_ds)}'
     )
 
     tb = TensorBoardLogger(log_dir=".logs", name='svd')
@@ -181,6 +225,17 @@ def main():
         callbacks=[tb]
     )
     model.save(os.path.join(CHECKPOINTS_ROOT, f'svd-aug{args.augmentation}-bs128-lr3-adamw-noam.ckpt'), weights_only=False)
+
+    prediction = model.predict(test_ds, batch_size=256, n_jobs=0)
+    prediction = np.vstack(list(prediction))
+    
+    targets = F.one_hot(test_ds[:]['speaker_1'], num_classes=630) + F.one_hot(test_ds[:]['speaker_2'], num_classes=630)
+    acc = metrics.accuracy_score(targets.cpu().detach().numpy(), prediction>=0.5)
+    ham = hamming_score(targets.cpu().detach().numpy(), prediction>=0.5)
+    print(
+        f'Accuracy: {acc}'
+        f'Hamming: {ham}'
+    )
 
 
 if __name__ == '__main__':
